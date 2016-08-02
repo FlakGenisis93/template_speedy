b06ac0a6d700ec460b99c4374ddeabe5b3a89c33
Conrad Urban <conrad.urban@t-online.de>
2016-08-02 13:52:01 +0200

    Removed dead data

--- hps_workspace/test/Debug/actuators/i2c.d
+++
@@ -1,9 +0,0 @@
-actuators/i2c.d: ../actuators/i2c.c ../actuators/i2c.h \
- C:\altera\16.0\embedded\ip\altera\hps\altera_hps\hwlib\include/soc_cv_av/socal/socal.h \
- C:\altera\16.0\embedded\ip\altera\hps\altera_hps\hwlib\include/soc_cv_av/socal/hps.h
-
-../actuators/i2c.h:
-
-C:\altera\16.0\embedded\ip\altera\hps\altera_hps\hwlib\include/soc_cv_av/socal/socal.h:
-
-C:\altera\16.0\embedded\ip\altera\hps\altera_hps\hwlib\include/soc_cv_av/socal/hps.h:
